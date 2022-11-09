f592cd46602dab55a4e384f79a6ef5164f71e043
VSTL <VSTL@DESKTOP-0CLRQ2I>
2022-11-09 12:27:42 +0530

    test

---
+++ src/test/java/com/java/Test/TestJavaProgram.java
@@ -0,0 +1,23 @@
+package com.java.Test;
+
+import org.testng.annotations.Test;
+
+import com.generic.BaseTest;
+import com.java.PageFactory.TestJava;
+
+public class TestJavaProgram extends BaseTest {
+	
+	private TestJava objTestJava;
+	public TestJavaProgram() {
+		objTestJava = new TestJava();
+	}
+	
+	
+	@Test
+	public void testJava()
+	{
+		objTestJava.openURLforFB();
+		System.out.println("Test");
+	}
+
+}
